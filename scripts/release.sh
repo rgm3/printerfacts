9c2e59d25f27340516d134f64a3debfe654c172d
Christine Dodrill <me@christine.website>
2020-07-09 16:55:47 -0400

    hack

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -p doctl -p kubectl
-nix-env -i -f ./nix/dhall.nix
 doctl kubernetes cluster kubeconfig save kubermemes
-dhall-to-yaml-ng < ./printerfacts.dhall | kubectl apply -n apps -f -
+dhall-to-yaml-ng < ../printerfacts.dhall | kubectl apply -n apps -f -
 kubectl rollout status -n apps deployment/printerfacts
