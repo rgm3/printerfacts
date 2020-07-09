b52297dad31c2567baa7fa32cf2d65de81691a5d
Christine Dodrill <me@christine.website>
2020-07-09 16:47:14 -0400

    fix

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ let
   pkgs = import <nixpkgs> { };
   printerfacts = import ./printerfacts.nix { inherit pkgs sources; };
 
-  name = "xena/printerfacts-warp";
+  name = "xena/printerfacts";
   tag = "latest";
 
 in pkgs.dockerTools.buildLayeredImage {
