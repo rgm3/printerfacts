aafe33afeeef24ff3b8e0e944390de1d71d8a6b9
Christine Dodrill <me@christine.website>
2020-07-09 17:15:32 -0400

    fix?

--- nix/dhall.nix
+++ nix/dhall.nix
@@ -1,5 +1,16 @@
 let
   sources = import ./sources.nix;
   pkgs = import sources.nixpkgs { };
-  dhall = import sources.easy-dhall-nix { inherit pkgs; };
-in dhall.dhall-yaml-simple
+in pkgs.stdenv.mkDerivation rec {
+  name = "dhall-yaml-simple";
+
+  src = sources.dhall-yaml;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    DHALL_TO_YAML=$out/bin/dhall-to-yaml-ng
+    install -D -m555 -T dhall-to-yaml-ng $DHALL_TO_YAML
+    mkdir -p $out/etc/bash_completion.d/
+    $DHALL_TO_YAML --bash-completion-script $DHALL_TO_YAML > $out/etc/bash_completion.d/dhall-to-yaml-completion.bash
+  '';
+}
