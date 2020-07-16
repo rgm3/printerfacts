2f8debe85930c43be30b589dd261c4d4a1953c20
Christine Dodrill <me@christine.website>
2020-07-16 16:10:45 -0400

    use ructe

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.2.0
+
+This site uses `ructe` instead of handlebars.
--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,70 @@
-use handlebars::Handlebars;
+use crate::templates::RenderRucte;
+use lazy_static::lazy_static;
 use pfacts::Facts;
+use prometheus::{opts, register_int_counter_vec, IntCounterVec};
 use rand::prelude::*;
-use serde::Serialize;
-use std::{convert::Infallible, sync::Arc};
-use warp::Filter;
+use std::convert::Infallible;
+use warp::{http::Response, Filter, Rejection, Reply};
+
+include!(concat!(env!("OUT_DIR"), "/templates.rs"));
+
+const APPLICATION_NAME: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+
+lazy_static! {
+    static ref HIT_COUNTER: IntCounterVec =
+        register_int_counter_vec!(opts!("hits", "Number of hits to various pages"), &["page"])
+            .unwrap();
+}
 
 async fn give_fact(facts: Facts) -> Result<String, Infallible> {
+    HIT_COUNTER.with_label_values(&["fact"]).inc();
     Ok(facts.choose(&mut thread_rng()).unwrap().clone())
 }
 
-#[derive(Serialize)]
-struct TemplateContext {
-    title: &'static str,
-    fact: Option<String>,
-    // This key tells handlebars which template is the parent.
-    parent: &'static str,
+async fn index(facts: Facts) -> Result<impl Reply, Rejection> {
+    HIT_COUNTER.with_label_values(&["index"]).inc();
+    Response::builder()
+        .html(|o| templates::index_html(o, facts.choose(&mut thread_rng()).unwrap().clone()))
 }
 
-struct WithTemplate<T: Serialize> {
-    name: &'static str,
-    value: T,
-}
-
-fn render<T>(template: WithTemplate<T>, hbs: Arc<Handlebars>) -> impl warp::Reply
-where
-    T: Serialize,
-{
-    let render = hbs
-        .render(template.name, &template.value)
-        .unwrap_or_else(|err| err.to_string());
-    warp::reply::html(render)
+async fn not_found() -> Result<impl Reply, Rejection> {
+    HIT_COUNTER.with_label_values(&["not_found"]).inc();
+    Response::builder()
+        .status(404)
+        .html(|o| templates::not_found_html(o))
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     pretty_env_logger::init();
     let facts = pfacts::make();
-    let mut hb = Handlebars::new();
-
-    hb.register_template_file("layout", "./templates/layout.hbs")?;
-    hb.register_template_file("footer", "./templates/footer.hbs")?;
-    hb.register_template_file("index", "./templates/index.hbs")?;
-    hb.register_template_file("error/404", "./templates/error/404.hbs")?;
 
     let fact = {
         let facts = facts.clone();
         warp::any().map(move || facts.clone())
     };
-    let hb = Arc::new(hb);
-    let handlebars = move |with_template| render(with_template, hb.clone());
 
     let files = warp::path("static").and(warp::fs::dir("./static"));
 
     let fact_handler = warp::get()
         .and(warp::path("fact"))
-        .and(fact)
+        .and(fact.clone())
         .and_then(give_fact);
 
     let index_handler = warp::get()
         .and(warp::path::end())
-        .map(move || WithTemplate {
-            name: "index",
-            value: TemplateContext {
-                title: "Printer Facts",
-                fact: {
-                    let ref facts = facts.clone();
-                    Some(facts.choose(&mut thread_rng()).unwrap().clone())
-                },
-                parent: "layout",
-            },
-        })
-        .map(handlebars.clone());
+        .and(fact.clone())
+        .and_then(index);
 
-    let not_found_handler = warp::any()
-        .map(move || WithTemplate {
-            name: "error/404",
-            value: TemplateContext {
-                title: "Not Found",
-                fact: None,
-                parent: "layout",
-            },
-        })
-        .map(handlebars.clone());
+    let not_found_handler = warp::any().and_then(not_found);
 
     log::info!("listening on port 5000");
     warp::serve(
         fact_handler
             .or(index_handler)
             .or(files)
-            .or(not_found_handler),
+            .or(not_found_handler)
+            .with(warp::log(APPLICATION_NAME)),
     )
     .run(([0, 0, 0, 0], 5000))
     .await;
