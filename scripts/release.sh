c9560224223e96e8e305bf9acc092c19ddc3cbbc
Cadey Ratio <cadey@noreply.example.org>
2022-02-27 12:52:39 +0000

    Delete 'scripts/release.sh'

--- scripts/release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -p doctl -p kubectl -i bash
-doctl kubernetes cluster kubeconfig save kubermemes
-dhall-to-yaml-ng < ./printerfacts.dhall | kubectl apply -n apps -f -
-kubectl rollout status -n apps deployment/printerfacts
